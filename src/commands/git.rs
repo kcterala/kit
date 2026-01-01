589d2f96a501a1f44999fd07c2bad795a6b0614f
Krishna Chaitanya Terala <kcterala@gmail.com>
2026-01-01 12:52:47 +0530

    feat: add clone arg which clones and also adds upstream (#3)
    * feat: add clone arg which clones and also adds upstream
    
    * pr comments

---
+++ src/commands/git.rs
@@ -0,0 +1,31 @@
+use std::process::{Command, ExitStatus};
+use anyhow::Result;
+use crate::commands::github::GetRepoResponse;
+
+
+pub fn clone_repository(repo_details: &GetRepoResponse) -> Result<ExitStatus> {
+    let clone_status = Command::new("git")
+        .args(&["clone", &repo_details.ssh_url])
+        .status()?;
+
+    if !clone_status.success() {
+        println!("Failed to clone repository");
+    }
+
+    Ok(clone_status)
+}
+
+pub fn add_upstream(repo_name: &str, parent_url: &str) -> Result<ExitStatus> {
+    let upstream_status = Command::new("git")
+        .args(&["remote", "add", "upstream", &parent_url])
+        .current_dir(repo_name)
+        .status()?;
+
+    if upstream_status.success() {
+        println!("Upstrem remote added: {}", parent_url);
+    } else {
+        println!("Failed to add upstream remote");
+    }
+
+    Ok(upstream_status)
+}
---
+++ src/commands/mod.rs
@@ -0,0 +1,55 @@
+use anyhow::{Ok, Result};
+
+use crate::commands::github::GetRepoResponse;
+
+mod github;
+mod git;
+
+pub fn clone_repository(repo: &str) -> Result<()> {
+    let (owner, repo_name) = match resolve(repo) {
+        Some(result) => result,
+        None => {
+            return Err(anyhow::anyhow!("Invalid repository URL format"));
+        }
+    };
+    let repo_details: GetRepoResponse = github::get_repo_details(&owner, &repo_name)?;
+    let clone_status = git::clone_repository(&repo_details)?;
+    if clone_status.success() && repo_details.fork {
+        println!("this repo is a fork, add parent url as remote upstream");
+        if let Some(parent) = repo_details.parent {
+            git::add_upstream(&repo_name, &parent.ssh_url)?;
+        }
+    }
+
+    Ok(())
+}
+
+pub fn fork_repository(repo: &str) -> Result<()> {
+    println!("Not implemented till now");
+    Ok(())
+}
+
+// https://github.com/kcterala/kcx.git
+// git@github.com:kcterala/kcx.git
+fn resolve(repo_url: &str) -> Option<(String, String)> {
+    if repo_url.starts_with("https://github.com/") {
+        let path = repo_url.strip_prefix("https://github.com/")?;
+        let path = path.strip_suffix(".git").unwrap_or(path);
+
+        let parts: Vec<&str> = path.split("/").collect();
+        if parts.len() == 2 {
+            return Some((parts[0].to_string(), parts[1].to_string()));
+        }
+    } else if repo_url.starts_with("git@github.com:") {
+          // Parse SSH URL
+          let path = repo_url.strip_prefix("git@github.com:")?;
+          let path = path.strip_suffix(".git").unwrap_or(path);
+
+          let parts: Vec<&str> = path.split("/").collect();
+          if parts.len() == 2 {
+              return Some((parts[0].to_string(), parts[1].to_string()));
+          }
+      }
+
+      None
+}
